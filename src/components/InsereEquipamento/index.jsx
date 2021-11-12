075a1197919b88a53ad3075cfeef81062b9ea281
Matheus Oliveira <matheusops.pc@gmail.com>
2021-11-12 15:42:13 -0400

    Alert fixed

--- src/components/InsereEquipamento/index.jsx
+++ src/components/InsereEquipamento/index.jsx
@@ -9,11 +9,10 @@ import * as React from 'react'
 import Snackbar from '@mui/material/Snackbar'
 import MuiAlert from '@mui/material/Alert'
 import { useState, useEffect } from 'react';
-// eslint-disable-next-line
-const baseURL = 'http://localhost:3333/equipamento'
+// const baseURL = 'http://localhost:3333/equipamento'
 
 
-export default function Item(){
+export default function Item() {
   // const [data, setTableData] = useState([]);
   const [fabricante, setFabricante] = useState([]);
   const [id_fabricante, setIdfabricante] = useState(0);
@@ -21,126 +20,159 @@ export default function Item(){
   const [id_modelo, setIdmodelo] = useState(0);
   const [prefixo, setPrefixo] = useState([]);
   const [cd_prefixo, setCdprefixo] = useState(0);
+// eslint-disable-next-line
+  const [msgSucess, setMsgSucess] = useState('');
+  // eslint-disable-next-line
+  const [msgFail, setMsgFail] = useState('');
+  const [isSucess, setIsSucess] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
 
-      const ress = async () => {
+    const ress = async () => {
 
-        const [
-          req_fabricante,
-          req_modelo,
-          req_prefixo
-        ] = await Promise.all([
-          axios.get("fabricante"),
-          axios.get("modelo"),
-          axios.get("prefixo"),
+      const [
+        req_fabricante,
+        req_modelo,
+        req_prefixo
+      ] = await Promise.all([
+        axios.get("fabricante"),
+        axios.get("modelo"),
+        axios.get("prefixo"),
 
-        ]);
-        setFabricante(req_fabricante.data);
-        setModelo(req_modelo.data);
-        setPrefixo(req_prefixo.data);
-        console.log(req_fabricante.data);
-        console.log(req_modelo.data);
-        console.log(req_prefixo.data);
+      ]);
+      setFabricante(req_fabricante.data);
+      setModelo(req_modelo.data);
+      setPrefixo(req_prefixo.data);
+      console.log(req_fabricante.data);
+      console.log(req_modelo.data);
+      console.log(req_prefixo.data);
+    }
+    ress();
+  }, []);
+
+  async function handleSubmit() {
+
+    // console.log();
+
+    try {
+      const res = await axios.post('equipamento', {
+        id_fabricante: id_fabricante,
+        id_modelo: id_modelo,
+        cd_prefixo: cd_prefixo
+      });
+      console.log('aqui e o erro' + res.data.error)
+      if(!res.data){
+        setIsSucess(false);
+        setMsgFail({
+          type: 'error',
+          message: "Não inserido"
+        })
+      }else{
+        setIsSucess(true);
+
+        setMsgSucess({
+          type: 'success', 
+          message: 'Adicionado com sucesso'
+        })
       }
-      ress();
-  },[]);
-  
-  async function  handleSubmit(){
+    } catch{
+      setIsSucess(false);
 
-  console.log('clicou!!!');
+      console.log('error catch');
+      setMsgFail({
+        type: 'error',
+        message: "Não inserido"
+      })
+    }
+    
+    //para da reload na pagina ao inserir
+    //  setTimeout(() => {  window.location.reload(); }, 6000); 
 
-  try {
-  const res =  await axios.post('equipamento',{
-      id_fabricante: id_fabricante,
-      id_modelo: id_modelo,
-      cd_prefixo:cd_prefixo
-    });
-  console.log(res.data); 
-  
-  } catch (error) {
-    console.log('Fail',error);
   }
 
-   //para da reload na pagina ao inserir
-   setTimeout(() => {  window.location.reload(); }, 6000); 
-  
- }
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  })
 
- const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props}/>;
-})
+  const [open, setOpen] = React.useState(false);
 
-const [open, setOpen] = React.useState(false);
-
-const handleOpen = () => {
-  setOpen(true)
-}
-
-const handleClose = (event, reason) => {
-  if (reason === 'clickaway') {
-    return;
+  const handleOpen = () => {
+    setOpen(true)
   }
-  setOpen(false);
-}
 
-  return(
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  }
+
+  return (
     <>
-    <Topbar />
+      <Topbar />
       <Title>Cadastrar Equipamento</Title>
 
-<Items onSubmit={(e)=>handleSubmit(e.preventDefault())}>
-      <Autocomplete className="item-id" 
-            id="Item"
-            options={fabricante}
-            getOptionLabel={(option) => option.id + " - "+ option.nm_fabricante }
-            onChange={(event, newValue) => {
-              setIdfabricante(newValue?.id);
-            }}
-            onSelect={() => {
-            }
-            }
-            style={{ width: 275 }}
-            renderInput={(params) => <TextField {...params} label="FABRICANTE..." variant="standard"/>}
-          />
-       
-       <Autocomplete className="item-id" 
-            id="Item"
-            options={modelo}
-            getOptionLabel={(option) => option.id + " - "+ option.nm_modelo }
-            onChange={(event, newValue) => {
-              setIdmodelo(newValue?.id);
-            }}
-            onSelect={() => {
-            }
-            }
-            style={{ width: 275 }}
-            renderInput={(params) => <TextField {...params} label="MODELO..." variant="standard"/>}
-          />
+      <Items onSubmit={(e) => handleSubmit(e.preventDefault())}>
+        <Autocomplete className="item-id"
+          id="Item"
+          options={fabricante}
+          getOptionLabel={(option) => option.id + " - " + option.nm_fabricante}
+          onChange={(event, newValue) => {
+            setIdfabricante(newValue?.id);
+          }}
+          onSelect={() => {
+          }
+          }
+          style={{ width: 275 }}
+          renderInput={(params) => <TextField {...params} label="FABRICANTE..." variant="standard" required/>}
+        />
 
-         <Autocomplete className="item-id" 
-            id="Item"
-            options={prefixo}
-            getOptionLabel={(option) => option.id + " - "+ option.cd_prefixo }
-            onChange={(event, newValue) => {
-              setCdprefixo(newValue?.cd_prefixo);
-            }}
-            onSelect={() => {
-            }
-            }
-            style={{ width: 275 }}
-            renderInput={(params) => <TextField {...params} label="PREFIXO..." variant="standard"/>}
-          />
+        <Autocomplete className="item-id"
+          id="Item"
+          options={modelo}
+          getOptionLabel={(option) => option.id + " - " + option.nm_modelo}
+          onChange={(event, newValue) => {
+            setIdmodelo(newValue?.id);
+          }}
+          onSelect={() => {
+          }
+          }
+          style={{ width: 275 }}
+          renderInput={(params) => <TextField {...params} label="MODELO..." variant="standard" required />}
+        />
+
+        <Autocomplete className="item-id"
+          id="Item"
+          options={prefixo}
+          getOptionLabel={(option) => option.id + " - " + option.cd_prefixo}
+          onChange={(event, newValue) => {
+            setCdprefixo(newValue?.cd_prefixo);
+          }}
+          onSelect={() => {
+          }
+          }
+          style={{ width: 275 }}
+          renderInput={(params) => <TextField {...params} label="PREFIXO..." variant="standard" required/>}
+        />
 
         <Button>
           <Strong className="fab-button" onClick={handleOpen} >Adicionar</Strong>
         </Button>
+        { isSucess ?
         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-         Cadastrado com Sucesso!
-        </Alert>
-      </Snackbar>
-</Items>
+          <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            Cadastrado com Sucesso!
+          </Alert>
+        </Snackbar>
+        :
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            Não Possivel Cadastrar!
+          </Alert>
+        </Snackbar>
+        }
+
+      </Items>
       <Footer>Flex&copy; - All Rights Reserved</Footer>
     </>
   )
--- src/components/LoginScreen/style.js
+++ src/components/LoginScreen/style.js
@@ -11,13 +11,15 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import axios from 'axios'
-import img from '../../styles/img/blue.png'
-import styled from "styled-components";
 import Snackbar from '@mui/material/Snackbar'
 import MuiAlert from '@mui/material/Alert'
+import axios from '../../services/api'
+import img from '../../styles/img/blue.png'
+import styled from "styled-components";
+// import Snackbar from '@mui/material/Snackbar'
+// import MuiAlert from '@mui/material/Alert'
 
-const baseURL = 'http://localhost:3333/'
+// const baseURL = 'http://localhost:3333/'
 
 export const Background = styled.div`
   background: #eee;
@@ -51,8 +53,8 @@ export const Title = styled.h2`
 export function InputWithIcon() {
   const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
-  const [sucess, setSucess] = useState(false);
   const history = useHistory();
+  const [isSucess, setIsSucess] = useState(true);
   
   const handleUser = e => {
     const usuario = e.target.value;
@@ -66,36 +68,44 @@ export function InputWithIcon() {
     console.log('handlesenha');
   }
 
-  const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props}/>;
-  })   
-
-  const handleSubmit = e=> {
+  async function handleSubmit(e) {
     e.preventDefault();
-    try{
-      console.log(user,password);
-      axios.post(`${baseURL}authenticate`, {
+
+    try {
+      const res = await axios.post('authenticate', {
       userText:user,
       passwordText:password,
-
-    }).then((response) => {
-      console.log('passou aqui 2')
-      history.push('/menu');
-      
     })
-    console.log('passou aqui 3')
-    
+    if(!!res.data){
+      history.push('/menu');
+      setIsSucess(true);
+    }else{
+      setIsSucess(false);
     }
-    catch(error){
-      console.log(error);
-      
-    }  
+  } catch{
+    setIsSucess(false);
+    console.log('error catch');
+  } finally {
+    handleOpen();
+  }
+}
 
-    console.log('passou aqui 1')
-    
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  })
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true)
   }
 
-  
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  } 
 
 
   const [values, setValues] = React.useState({
@@ -116,58 +126,16 @@ export function InputWithIcon() {
 
   //ALERTA////////////////////////////////////
 
-  function FlashMessage(){
-    const [open, setOpen] = useState(true);
-    const handleClose = (event, reason) => {
-      if (reason === 'clickaway') {
-        return;
-      }
-
-      setOpen(false);
-    }
-
-    return (
-      <>
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
-            Usuário/Senha inválidos!
-          </Alert>
-        </Snackbar>
-      </>
-    );
-  }
-
-  // function FlashSucess(){
-  //   const [open, setOpen] = useState(true);
-  //   const handleClose = (event, reason) => {
-  //     if (reason === 'clickaway') {
-  //       return;
-  //     }
-
-  //     setOpen(false);
-  //   }
-
-  //   return (
-  //     <>
-  //       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-  //         <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
-  //           Bem-Vindo!
-  //         </Alert>
-  //       </Snackbar>
-  //     </>
-  //   );
-  // }
-
   return (
     <>
     <Background>
     <Logo src={img}/>
     <Card onSubmit={handleSubmit}>
     <Box component="form" sx={{'& > :not(style)': { m: 1, width: '310px' },}} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Username" variant="outlined" value={user} onChange={handleUser} />
+      <TextField required id="outlined-basic" label="Username" variant="outlined" value={user} onChange={handleUser} />
     </Box>      
       <FormControl sx={{ m: 1, width: '310px' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password" >Password</InputLabel>
+          <InputLabel htmlFor="outlined-adornment-password" required>Password</InputLabel>
           <OutlinedInput
             id="outlined-adornment-password"
             type={values.showPassword ? 'text' : 'password'}
@@ -189,13 +157,25 @@ export function InputWithIcon() {
           />
         </FormControl>
         <Box component="form" sx={{'& > :not(style)': { m: 1, width: '310px' },}} noValidate autoComplete="off">    
+
            <Button sx={{m: 1, widht: '300px'}} variant="contained" type="submit" onClick={handleSubmit}>Login</Button>
+
         </Box>
-    </Card>
-            {
-              !sucess ? '' : <FlashMessage />
-            }
-            
+
+        { isSucess ?
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            Cadastrado com Sucesso!
+          </Alert>
+        </Snackbar>
+        :
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            Usuário/Senha Inválido!
+          </Alert>
+        </Snackbar>
+        }
+    </Card>           
     </Background>
         </>
   );
